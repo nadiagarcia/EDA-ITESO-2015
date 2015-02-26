67e434a669575ae44826574fd318c3cd3ae90c3d
Nadia Paola Garcia <ngarcia@tacitknowledge.com>
2015-02-26 11:11:13 -0600

    agregando colas.h y correccion a pilas.h

--- Utilities/pilas.h
+++ Utilities/pilas.h
@@ -3,51 +3,61 @@
 
 typedef int ELEMENTO;
 
-typedef struct Pila{
+typedef struct Pila {
 	ELEMENTO iValor;
 	struct Pila siguiente;
-}PILA;
+} PILA;
 
-int isEmpty(PILA *stack){
-	if(stack == NULL)
+int isEmpty(PILA *stack) {
+	if (stack == NULL)
 		return 1;
 	else
 		return 0;
 }
 
-void empty(PILA **stack){
-	if(stack != NULL){
+void empty(PILA **stack) {
+	if (stack != NULL) {
 		PILA *aux;
-		while(*stack != NULL){
+		while (*stack != NULL) {
 			aux = (*stack)->siguiente;
 			free(*stack);
 			*stack = aux;
 		}
-	}		
+	}
 }
 
-ELEMENTO top(PILA *stack){
-	return stack->valor;
+ELEMENTO top(PILA *stack) {
+
+	if (stack == NULL) {
+		pritnf("Error, pila vacia");
+		return -1; //valor que indica un error al hacer top
+	}
+
+	return stack->iValor;
 }
 
-ELEMENTO pop(PILA **stack){
+ELEMENTO pop(PILA **stack) {
+
+	if (*stack == NULL) {
+		pritnf("Error, pila vacia");
+		return -1; //valor que indica un error al hacer top
+	}
 
 	ELEMENTO valor;
-		
+
 	PILA *aux = (*stack)->siguiente;
-	valor = (*stack)->valor;
-	
+	valor = (*stack)->iValor;
+
 	free(*stack);
 	*stack = aux;
-	
+
 	return valor;
 }
 
-void push(PILA **stack, ELEMENTO e){
+void push(PILA **stack, ELEMENTO e) {
 	PILA *nuevo = (PILA *) malloc(sizeof(PILA));
-	nuevo->valor = e;
+	nuevo->iValor = e;
 	nuevo->siguiente = *stack;
 	*stack = nuevo;
 }
-
 
