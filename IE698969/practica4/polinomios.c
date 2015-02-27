543ae820081e1b9102d6b1b1c50705b735c2ca6e
Felipe Bojórquez García <github@fbg.mx>
2015-02-27 08:19:18 -0600

    creacion de proyecto para practica 4

---
+++ IE698969/practica4/polinomios.c
@@ -0,0 +1,23 @@
+/*
+ * polinomios.c
+ *
+ *	Descripcion:
+ *	Realiza un programa que, usando listas, evalue
+ *	un polinomio de una sola variable.
+ *
+ *	La entrada, proporcionada por el usuario será
+ *	el coeficiente y su exponente para describir cada termino.
+ *	Cuando haya terminado de ingresar los términos del polinomio
+ *	el programa le preguntará con qué valores evaluará el polinomio.
+ *	El programa debe de evaluar el polinomio con
+ *	los valores dados por el usuario.
+ *
+ *  Created on: 27/02/2015
+ *      Author: ie698969
+ */
+
+
+int main{
+	/*Codigo*/
+	return 0;
+}
