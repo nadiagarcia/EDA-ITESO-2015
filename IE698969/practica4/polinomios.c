c6f341f18fdc6bc77673113abd64b3bd488b58da
Felipe Bojórquez García <felipe@MacBookPro54.lan>
2015-02-28 16:18:00 -0600

    Practica 4 Polinomios funcionando

--- IE698969/practica4/polinomios.c
+++ IE698969/practica4/polinomios.c
@@ -7,7 +7,7 @@
  *
  *	La entrada, proporcionada por el usuario será
  *	el coeficiente y su exponente para describir cada termino.
- *	Cuando haya terminado de ingresar los términos del polinomio
+ *	Cuando haya terminado de ingresar los terminos del polinomio
  *	el programa le preguntará con qué valores evaluará el polinomio.
  *	El programa debe de evaluar el polinomio con
  *	los valores dados por el usuario.
@@ -21,86 +21,89 @@
 #include <math.h> //Se agrega para operaciones matematicas
 //Se crea una lista para guardar cada termino del polinomio
 typedef struct nodo {
-	int coeficiente;
-	int exponente;
-	struct nodo * siguiente;
+    int coeficiente;
+    int exponente;
+    struct nodo * siguiente;
 } TERMINO;
 
 int main() {
-	//Codigo eclipse
-	setvbuf(stdout, NULL, _IONBF, 0);
-
-	//Inicializar lista
-	TERMINO *inicio = NULL;
-	TERMINO *nuevo = (TERMINO *) malloc(sizeof(TERMINO));
-	TERMINO *actual;
-
-	//Variables
-	int variableX, seleccion, salir = 1, resultado;
-
-	while (salir) {
-		printf("Calcular Polinomio.\n");
-		printf("Seleccione\n");
-		printf("1. Agregar termino al polinomio.\n");
-		printf("2. Evaluar polinomio.\n");
-		printf("3. Salir.\n");
-
-		scanf("%d", &seleccion);
-
-		switch (seleccion) {
-
-		case 1:
-			//Inicializar nuevo
-			nuevo = (TERMINO *) malloc(sizeof(TERMINO));
-			nuevo->siguiente = NULL;
-
-			//Agregar termino al polinomio
-			printf("Ingrese coeficiente.\n");
-			scanf("%d", &nuevo->coeficiente);
-			printf("Ingrese exponente.\n");
-			scanf("%d", &nuevo->exponente);
-
-			if (inicio == NULL) {
-				inicio = nuevo;
-			} else {
-				actual = inicio;
-				nuevo->siguiente = inicio;
-				inicio = nuevo;
-				printf("Elemento agregado\n");
-			}
-			break;
-
-		case 2:
-			//Evaluar polinomio
-			resultado = 0;
-
-			//Ingresar el valor de vaeriableX
-			printf("Ingrese el valor de X\n");
-			scanf("%d", &variableX);
-
-			//Operaciones
-			actual = inicio;
-			if (actual != NULL) {
-				while (actual->siguiente != NULL) {
-					//Hacer operaciones e ir guardando en resultado
-					resultado = resultado + ((pow(variableX, actual->exponente)) * (actual->coeficiente));
-				}
-			} else {
-				printf("No hay elementos en el polinomio.");
-			}
-			printf("Resultado: %d", resultado);
-			break;
-
-		case 3:
-			//Salir
-			salir = 0;
-			break;
-
-		default:
-			printf("Seleccione una opcion valida.\n");
-			break;
-		}
-
-	}
-	return 0;
+    //Codigo eclipse
+    setvbuf(stdout, NULL, _IONBF, 0);
+    
+    //Inicializar lista
+    TERMINO *inicio = NULL;
+    TERMINO *nuevo = (TERMINO *) malloc(sizeof(TERMINO));
+    TERMINO *actual;
+    
+    //Variables
+    int variableX, seleccion, salir = 1, resultado;
+    
+    printf("Calculadora de Polinomios.\n");
+    
+    while (salir) {
+        printf("Seleccione\n");
+        printf("1. Agregar termino al polinomio.\n");
+        printf("2. Evaluar polinomio.\n");
+        printf("3. Salir.\n");
+        
+        scanf("%d", &seleccion);
+        
+        switch (seleccion) {
+                
+            case 1:
+                //Inicializar nuevo
+                nuevo = (TERMINO *) malloc(sizeof(TERMINO));
+                nuevo->siguiente = NULL;
+                
+                //Agregar termino al polinomio
+                printf("Ingrese coeficiente.\n");
+                scanf("%d", &nuevo->coeficiente);
+                printf("Ingrese exponente.\n");
+                scanf("%d", &nuevo->exponente);
+                nuevo->siguiente = NULL;
+                
+                if (inicio == NULL) {
+                    inicio = nuevo;
+                } else {
+                    actual = inicio;
+                    nuevo->siguiente = inicio;
+                    inicio = nuevo;
+                    printf("Elemento agregado\n");
+                }
+                break;
+                
+            case 2:
+                //Evaluar polinomio
+                resultado = 0;
+                
+                //Ingresar el valor de vaeriableX
+                printf("Ingrese el valor de X\n");
+                scanf("%d", &variableX);
+                
+                //Operaciones
+                actual = inicio;
+                if (actual != NULL) {
+                    while (actual != NULL) {
+                        //Hacer operaciones e ir guardando en resultado
+                        resultado = resultado + ((pow(variableX, actual->exponente)) * (actual->coeficiente));
+                        actual = actual->siguiente;
+                    }
+                    printf("Resultado: %d\n", resultado);
+                } else {
+                    printf("No hay elementos en el polinomio.\n");
+                }
+                break;
+                
+            case 3:
+                //Salir
+                salir = 0;
+                break;
+                
+            default:
+                printf("Seleccione una opcion valida.\n");
+                break;
+        }
+        
+    }
+    return 0;
 }
