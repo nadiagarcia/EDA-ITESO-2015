a8ff6034e20db26648dee0b7d1b14c0fc855f98c
Felipe Bojórquez García <felipe@MacBookPro54.lan>
2015-02-28 18:05:16 -0600

    Crear proyecto practica 5

---
+++ IE698969/practica5Palabras1/practica5Palabras1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  practica5Palabras1
+//
+//  Created by Felipe on 2/28/15.
+//  Copyright (c) 2015 Felipe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
