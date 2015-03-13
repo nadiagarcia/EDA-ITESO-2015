d6df72b3b41ac9a61f907b5ef6a14a964c9369b7
Felipe Bojórquez García <github@fbg.mx>
2015-03-13 08:45:19 -0600

    ocho reinas dos

--- IE698969/8reinas/reinas.c
+++ IE698969/8reinas/reinas.c
@@ -1,19 +1,16 @@
 /*
  * reinas.c
  *
- *  Colocar 8 reinas en un tablero de ajedrez sin que se ataquen
+ * Colocar 8 reinas en un tablero de ajedrez sin que se ataquen
  *
- *  Created on: 13/03/2015
- *      Author: ie698969
+ * Created on: 13/03/2015
+ * Author: ie698969
  */
-
 #include <stdio.h>
-
 #define SIZE 8
-
-int tablero[SIZE][SIZE] = { { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 } };
+int tablero[SIZE][SIZE] = { { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 }, { 0 },
+		{ 0 } };
 //inicializa tablero todos cero. Dos dimensiones.
-
 void llenarAtaque(int i, int j, int num) { //Lugares que ataca la reina colocada (funcion para ahorrar lineas).
 	int fila;
 	for (fila = 0; fila < SIZE; fila++) { //Llenar vertical
@@ -27,32 +24,32 @@ void llenarAtaque(int i, int j, int num) { //Lugares que ataca la reina colocada
 			tablero[i][columna] = num;
 		}
 	}
-	//LLenar diagonales, izquierda arriba
-	fila = 1, columna = j;
+//LLenar diagonales, izquierda arriba
+	fila = i, columna = j;
 	while (fila >= 0 && columna >= 0) {
 		if (tablero[fila][columna] == 0) {
 			tablero[fila][columna] = num;
 		}
 		fila--, columna--;
 	}
-	//LLenar diagonales, derecha arriba
-	fila = 1, columna = j;
+//LLenar diagonales, derecha arriba
+	fila = i, columna = j;
 	while (fila >= 0 && columna < SIZE) {
 		if (tablero[fila][columna] == 0) {
 			tablero[fila][columna] = num;
 		}
 		fila--, columna++;
 	}
-	//LLenar diagonales, izquierda abajo
-	fila = 1, columna = j;
+//LLenar diagonales, izquierda abajo
+	fila = i, columna = j;
 	while (fila < SIZE && columna >= 0) {
 		if (tablero[fila][columna] == 0) {
 			tablero[fila][columna] = num;
 		}
 		fila++, columna--;
 	}
-	//LLenar diagonales, derecha abajo
-	fila = 1, columna = j;
+//LLenar diagonales, derecha abajo
+	fila = i, columna = j;
 	while (fila < SIZE && columna < SIZE) {
 		if (tablero[fila][columna] == 0) {
 			tablero[fila][columna] = num;
@@ -60,7 +57,6 @@ void llenarAtaque(int i, int j, int num) { //Lugares que ataca la reina colocada
 		fila++, columna++;
 	}
 }
-
 void limpiarAtaque(int num) {
 	int i, j;
 	for (i = 0; i < SIZE; i++) {
@@ -72,7 +68,6 @@ void limpiarAtaque(int num) {
 		printf("\n");
 	}
 }
-
 void imprimirTablero() {
 	int i, j;
 	for (i = 0; i < SIZE; i++) {
@@ -83,7 +78,6 @@ void imprimirTablero() {
 	}
 	printf("\n===================\n");
 }
-
 int colocarReina(int reina) {
 	if (reina == SIZE + 1) { //Cuando intente poner una reina de mas ya terminamos.
 		imprimirTablero();
@@ -103,10 +97,10 @@ int colocarReina(int reina) {
 		return 0; //Sin solucion
 	}
 }
-
 int main() {
 	setvbuf(stdout, NULL, _IONBF, 0); //Codigo eclipse
-	//Tablero de ajedrez
+//Tablero de ajedrez
+	printf("Hola");
 	colocarReina(1); //Empezamos con la reina 1
 	return 0;
 }
