da5efc5a505f4f6908fc88e746a9cfa4999a2db8
DantePe <dant_elp@hotmail.com>
2015-03-13 08:56:49 -0600

    prueba

--- nt699567/recursividad2/recursividad2.c
+++ nt699567/recursividad2/recursividad2.c
@@ -4,5 +4,28 @@
  *  Created on: 10/03/2015
  *      Author: nt699567
  */
+#include <stdio.h>
 
+int digitos(numero) {
 
+	int n = 1;
+
+	if (numero % n == 0) {
+		printf("%i", numero);
+		return 0;
+	}
+
+	return (numero + 1) + digitos();
+}
+
+int main() {
+
+	setvbuf(stdout,NULL,_IONBF,0);
+
+	int i, numero1=1;
+
+	for (i = 0; i < 987654321; i++) {
+		digitos(numero1);
+	}
+	return 1;
+}
